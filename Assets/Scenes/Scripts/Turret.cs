c957a0af41598e4e00845ab2f394e50a1d51e7a4
Sean-Sanii-Nejad <sean-saniinejad@hotmail.co.uk>
2023-08-07 14:28:46 +0100

    build

--- Assets/Scenes/Scripts/Turret.cs
+++ Assets/Scenes/Scripts/Turret.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.EditorTools;
+
 using UnityEngine;
 
 public class Turret : MonoBehaviour
